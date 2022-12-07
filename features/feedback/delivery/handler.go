b31304281dc2db21138ea7c57d6f9ea505bb9f77
Anti-Gen <putrategu@gmail.com>
2022-12-07 22:31:18 +0700

    id

--- features/feedback/delivery/handler.go
+++ features/feedback/delivery/handler.go
@@ -1,8 +1,23 @@
 package delivery
 
-// type FeedbackDelivery struct {
-// 	feedbackServices feedback.ServiceInterface
-// }
+// import (
+// 	"be13/project/features/feedback"
+
+// 	"github.com/Labstack/echo"
+// )
+
+// // type FeedbackDelivery struct {
+// // 	feedbackServices feedback.ServiceInterface
+// // }
+
+// // func NewFeedback(service feedback.ServiceInterface, e *echo.Echo) {
+// // 	handler := &FeedbackDelivery{
+// // 		feedbackServices: service,
+// // 	}
+
+// // 	e.POST("/feedback", handler.AddClass)
+// // 	e.PUT("/feedback", handler.UpdateClass)
+// // 	e.DELETE("feedback", handler.DeleteClass)
 
 // func NewFeedback(service feedback.ServiceInterface, e *echo.Echo) {
 // 	handler := &FeedbackDelivery{
