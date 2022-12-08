1ff7a6e782cc3d7306f07e10146564393bc6b352
Anti-Gen <putrategu@gmail.com>
2022-12-08 19:18:28 +0700

    teguh-final

--- features/feedback/delivery/response.go
+++ features/feedback/delivery/response.go
@@ -2,28 +2,23 @@ package delivery
 
 import (
 	"be13/project/features/feedback"
-	"time"
 )
 
 type FeedbackResponse struct {
-	ID        uint   `json:"feedback_id"`
-	UserID    uint   `json:"user_id"`
-	Status    string `json:"status"`
-	MenteeID  uint   `json:"mentee_id"`
-	Comment   string `json:"comment"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID       uint   `json:"feedback_id"`
+	UserID   uint   `json:"user_id"`
+	Status   string `json:"status"`
+	MenteeID uint   `json:"mentee_id"`
+	Comment  string `json:"comment"`
 }
 
 func CoreToResponse(core feedback.Core) FeedbackResponse {
 	response := FeedbackResponse{
-		ID:        core.ID,
-		UserID:    core.UserID,
-		Status:    core.Status,
-		MenteeID:  core.MenteeID,
-		Comment:   core.Comment,
-		CreatedAt: core.CreatedAt,
-		UpdatedAt: core.UpdatedAt,
+		ID:       core.ID,
+		UserID:   core.UserID,
+		Status:   core.Status,
+		MenteeID: core.MenteeID,
+		Comment:  core.Comment,
 	}
 	return response
 
